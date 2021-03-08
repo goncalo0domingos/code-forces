27ab6776f20aa9900cdc9232122bf4fa2ddffadf
KeichITensei <quartime02@gmail.com>
2021-03-08 04:23:54 +0000

    A - Anti-knapsack - Mastered

--- Test_Exercises/A.py
+++ Test_Exercises/A.py
@@ -1,30 +1,13 @@
 def solve(n,k):
-    number_set = [i for i in range(1,n+1)]
-    number_set.pop(k-1)
-    count = 0
-    for i in range(0,k-1):
-        a = number_set[i]
-        if a:
-            for j in range(k-2-i,0,-1):
-                b = number_set[j]
-                if b and (a != b):
-                    if (a + b) == k:
-                        number_set[i] = None
-                        count += 1
-                        break
-                    elif (a + b) < k: break
-
-    print((n-1-count))
-    for i in number_set:
-        if i:
-            print(i,end=' ')
-    print()
+    number_set = list(range((k+1) // 2,k)) + list(range(k+1,n+1))
+    print(len(number_set))
+    print(" ".join(map(str, number_set)))
 
 def anti_knapsack():
-    for i in range(0,int(input())):
-        try:
+    for _ in range(int(input())):
             n,k = input().split()
             solve(int(n),int(k))
-        except:
-            return
+
 anti_knapsack()
+
+
